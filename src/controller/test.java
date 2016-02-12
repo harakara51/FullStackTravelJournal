17d33ea7ae2e25032f2f9b6cbc0395090a5f6aad
harakara51 <harakara51@gmail.com>
2016-02-12 16:52:21 -0700

    teat

--- src/controller/test.java
+++ src/controller/test.java
@@ -3,8 +3,10 @@ package controller;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import Entities.User;
+
 
 public class test
 {
@@ -14,9 +16,26 @@ public class test
 		EntityManagerFactory emf= Persistence.createEntityManagerFactory("Travel");
 		System.out.println("after Factory");
 		EntityManager em = emf.createEntityManager();
-		System.out.println("after EM");
+		System.out.println("after EM"); 
 		
-		User Travis = new User("Travos","dsfalf@fladsjf.com","123334");
+		User Travis = new User("Travis","dsfalf@fladsjf.com","1234");
+		
+		
+	//	TypedQuery<User> query = em.createQuery 
+//                ("select U from User U where U.username = ",
+//                 User.class);
+		
+		String query = "select U from User U where U.username = ";
+//      TypedQuery<Job> tq = em.createQuery(query, Job.class);
+//      List<Job> jobs = tq.getResultList();
+      
+	
+//		String query = "SELECT U FROM User U where e.username =";
+//		TypedQuery test = em.createQuery(query, User.class);
+//      List<Job> jobs = tq.getResultList();
+		
+		
+	//	System.out.println(temp.getPassword());
 //		Travis.setUsername("TRAVIS");
 		
 		em.getTransaction().begin();
