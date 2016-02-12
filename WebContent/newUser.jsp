6e9fa2fea0df04bcae03a85ee694504f778f7beb
harakara51 <harakara51@gmail.com>
2016-02-12 10:02:12 -0700

    sdf

---
+++ WebContent/newUser.jsp
@@ -0,0 +1,39 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+
+<html lang="en">
+<head>
+ <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+ <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+ <title>Personal website </title>
+​
+ <!-- CSS  -->
+ <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+ <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+ <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+​
+<body>
+     
+​
+<div class="row" id ="test">
+        
+          <div class="card indigo lighten-4" id="newUser">
+           
+           <h1> Creating user user</h1>
+            </div>
+
+</div>
+</div>
+​
+
+​
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+ <script src="js/materialize.js"></script>
+ <script src="js/jquery.lettering.js"></script>
+</body>
+<footer>
+	
+</footer>
---
+++ src/controller/LoginController.java
@@ -0,0 +1,74 @@
+package controller;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+@Controller
+@SessionAttributes({ "username","password"})
+public class LoginController
+{
+	
+	@ModelAttribute("username")
+	public String createUserName()
+	{
+		String username="";
+		return username;
+	}
+	
+	@ModelAttribute("password")
+	public String createPassword()
+	{
+		String password="";
+		return password;
+	}
+	
+	
+	@RequestMapping(path = "GetUserTravel.do", params = "username", method = RequestMethod.POST)
+	public ModelAndView getByName(@RequestParam("username") String username, @RequestParam("password") String password)
+	{
+		System.out.println("in user login controller");
+		ModelAndView mv = new ModelAndView();
+		// System.out.println("In controller and size of array is" +
+		// NovelDAO.getNovelByLanguage(language).size());
+		mv.setViewName("dashboard.jsp");
+
+		mv.addObject("username", username);
+		
+
+		return mv;
+	}
+
+	
+	@RequestMapping(path = "CreateUserTravel.do",  method = RequestMethod.POST)
+	public ModelAndView createUser()
+	{
+		
+		System.out.println("inside creating new user method");
+		Boolean newUser =true;
+		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("userNew", newUser);
+		// System.out.println("In controller and size of array is" +
+		// NovelDAO.getNovelByLanguage(language).size());
+		mv.setViewName("newUser.jsp");
+		
+		
+
+		
+		
+
+		return mv;
+	}
+	
+	
+
+}
